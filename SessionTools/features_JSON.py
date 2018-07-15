bc8aa0f8aab15dd704fd34f836464d5e7397c08e
Luke Church <luke@church.name>
2018-07-15 16:11:37 +0100

    Add structure for feature extractors for JSON files

--- SessionTools/features_JSON.py
+++ SessionTools/features_JSON.py
@@ -1,3 +1,5 @@
+# Feature extractors for JSON files (Dynamo 2+)
+
 import json
 
 def getVersion(b64decodedData):
@@ -9,3 +11,27 @@ def getVersion(b64decodedData):
 def usesListAtLevel(data):
     usesList = data.find('"UseLevels": true') > -1
     return usesList
+
+def hasHiddenNodes(data):
+    return data.find('"ShowGeometry": false,') > -1     # TO CONFIRM
+
+def hasUpstreamHiddenNodes(data):
+    return False                                        # TODO
+
+def hasShortestLacing(data):
+    return False                                        # TODO
+
+def hasLongestLacing(data):
+    return False                                        # TODO
+
+def hasDisabledLacing(data):
+    return data.find('"Replication": "Disabled"') > -1  # TO CONFIRM
+
+def hasCrossProductLacing(data):
+    return False                                        # TODO
+
+def hasPinned(data):
+    return False                                        # TODO
+
+def hasFrozen(data):
+    return False                                        # TODO
